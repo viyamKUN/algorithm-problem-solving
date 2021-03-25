40578f6e239f05d26bd79d97b7d644cc57630a3c
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-03-25 19:53:36 +0900

    c++ 프로젝트 생성

---
+++ cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 윤하연 on 2021/03/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
